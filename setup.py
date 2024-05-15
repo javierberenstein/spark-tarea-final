1b9a7baa3a1096f88eee02abd08e81599aebaf3d
Javier Berenstein <javier.berenstein@adsmurai.com>
2024-05-15 21:46:20 +0200

    tarea final

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from setuptools import setup, find_packages
 setup(
     name="motor-ingesta",
     version="0.1.0",
-    author="Nombre del alumno",
-    author_email="alumno@ucm.es",
+    author="Javier Berenstein Rey",
+    author_email="javier.berenstein@gmail.com",
     description="Motor de ingesta para el curso de Spark",
     long_description="Motor de ingesta para el curso de Spark",
     long_description_content_type="text/markdown",
-    url="https://github.com/nombrealumno",
+    url="https://github.com/javierberenstein",
     python_requires=">=3.8",
     packages=find_packages(),
     package_data={"motor_ingesta": ["resources/*.csv"]}
